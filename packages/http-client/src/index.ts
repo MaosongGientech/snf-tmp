8596e07e8a10b8e726ce96c349db5210b62102a9
毛松 <song.mao@gientech.com>
2025-12-17 19:12:26 +0800

    add index.ts to http-client

---
+++ packages/http-client/src/index.ts
@@ -0,0 +1,6 @@
+import HttpClient from "./httpClient.js"
+import HttpClientError from "./httpClientError.js"
+
+export { HttpClient as default, HttpClientError }
+export * from "./types.js"
+export * from "./utils.js"
